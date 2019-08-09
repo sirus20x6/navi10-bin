c9c395a4b1df045cb1a196630262b40c4d2f8b07
sirus20x6 <sirus20x6@users.noreply.github.com>
2019-08-08 21:14:04 -0500

    Add files via upload
    added script

---
+++ amdbin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_asd.bin -o /lib/firmware/amdgpu/navi10_asd.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_ce.bin -o /lib/firmware/amdgpu/navi10_ce.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_gpu_info.bin -o /lib/firmware/amdgpu/navi10_gpu_info.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_me.bin -o /lib/firmware/amdgpu/navi10_me.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_mec.bin -o /lib/firmware/amdgpu/navi10_mec.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_mec2.bin -o /lib/firmware/amdgpu/navi10_mec2.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_pfp.bin -o /lib/firmware/amdgpu/navi10_pfp.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_rlc.bin -o /lib/firmware/amdgpu/navi10_rlc.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_sdma.bin -o /lib/firmware/amdgpu/navi10_sdma.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_sdma1.bin -o /lib/firmware/amdgpu/navi10_sdma1.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_smc.bin -o /lib/firmware/amdgpu/navi10_smc.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_sos.bin -o /lib/firmware/amdgpu/navi10_sos.bin
+sudo curl https://people.freedesktop.org/~agd5f/radeon_ucode/navi10/navi10_vcn.bin -o /lib/firmware/amdgpu/navi10_vcn.bin
+
